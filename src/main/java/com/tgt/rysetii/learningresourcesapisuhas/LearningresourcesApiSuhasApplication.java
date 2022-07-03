899244af0b1bca07fb314da39157ef0073ebacaa
SuhasPadi012 <suhaspadi@gmail.com>
2022-07-03 13:54:30 +0530

    Added HelloWorld to the first appilication

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapisuhas/LearningresourcesApiSuhasApplication.java
@@ -0,0 +1,14 @@
+package com.tgt.rysetii.learningresourcesapisuhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiSuhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiSuhasApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/learningresourcesapisuhas/LearningresourcesApiSuhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapisuhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiSuhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
